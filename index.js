c08fa9024f8189eb18d47846192be73bdd9ce66f
Vadim Sidorov <vadsdrv@yandex.ru>
2021-11-14 12:36:11 +0300

    refactor: clear main branch

--- index.js
+++
@@ -1,6 +0,0 @@
-import pathsCheck from './src/pathsCheck.js';
-import cliArgumentsCheck from './src/cliArgumentsCheck.js';
-
-const streamsArr = pathsCheck();
-
-cliArgumentsCheck(streamsArr);
--- src/options.js
+++
@@ -1,4 +0,0 @@
-export class Options {
-    static shift = 0;
-    static cipher = '';
-}
