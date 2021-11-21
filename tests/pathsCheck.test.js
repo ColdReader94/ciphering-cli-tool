71c80ff4a05f87bbee30b28507c35e08ecee2ed1
Vadim Sidorov <vadsdrv@yandex.ru>
2021-11-21 19:14:23 +0300

    fix: pathCheck process exit test

--- tests/pathsCheck.test.js
+++ tests/pathsCheck.test.js
@@ -2,6 +2,7 @@ import pathsCheck from "../src/pathsCheck.js";
 
 describe('Provided paths check', () => {
    const mockConsoleOutput = jest.spyOn(process, 'exit').mockReturnValue(0);
+   const mockConsoleOutputWrong = jest.spyOn(process, 'exit');
 
     it('should throw repeated arguments error', () => {
        process.argv = ['node', 'index.js', '-c', 'A-C1', '-c', 'A-R0', '-i', 'input.txt', '-o', 'output.txt']      
@@ -22,7 +23,7 @@ describe('Provided paths check', () => {
        done()}).toThrowError("ENOENT: no such file or directory, stat 'dfsasfdaf.txt'");
     })
 
-    it('should execute ok', () => {
+   it('should execute ok', () => {
       process.argv = ['node', 'index.js', '-c', 'A-C1', '-i', 'input.txt', '-o', 'output.txt'];
       expect(
          mockConsoleOutput()).toBe(0)
